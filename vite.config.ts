202db9ff13cf448ef7e5b6c98a0b8a88343d64f3
Santiago Ariel <santiagoarielv98@gmail.com>
2023-09-18 01:56:07 -0300

    config tailwind

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const junior = resolve(__dirname, "src/junior");
+
+export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/frontend-mentor-challenges/" : "/",
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      input: {
+        main: resolve("index.html"),
+        other: resolve(junior, "other/index.html"),
+        newsletterSignUpWithSuccessMessage: resolve(junior, "newsletter-sign-up-with-success-message/index.html"),
+      },
+    },
+  },
+});
